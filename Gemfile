89a2be18ec4aa3d112aa3183dc62a44e43380330
Peter de Ruijter <peter@l2code.nl>
2013-05-17 21:13:55 +0200

    RAAAAIIIIIIIIIIILLLSSS!

--- Gemfile
+++ Gemfile
@@ -12,3 +12,9 @@ group :assets do
 end
 
 gem 'jquery-rails'
+
+group :development, :test do
+    gem 'rspec-rails', '~> 2.0'
+    gem 'capybara'
+    gem 'shoulda-matchers'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Liftizr::Application.config.secret_token = 'ec4e47960f40e3c16c48b2450c1784b7736068b42c43454ff9046835e6a3e47e230ed99a9e13a0ffb592d953c3600164f3397c43f90fc0e5a7f593ec6c11d2b0'
