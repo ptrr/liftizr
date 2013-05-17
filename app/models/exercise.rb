31d662e4dda3d355947feafa38c8f5d6168568b0
Peter de Ruijter <peter@l2code.nl>
2013-05-17 23:07:03 +0200

    I was wrong...

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,9 +1,11 @@
 class Exercise < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :exercise_type
+
   has_many :sequences
   belongs_to :training_day
 
   EXERCISE_TYPES = %w{deadlift squat benchpress inclined_benchpress declined_benchpress over_head_press shoulder_press incline_dumbell_press dumbell_press pull_up chin_up dips peck_fly cable_cross_over }
 
-  validates_presence_of :exercise_type, inclusion: { in: EXERCISE_TYPES }
+  validates_presence_of :exercise_type
+  validates :exercise_type, inclusion: { in: EXERCISE_TYPES }
 end
